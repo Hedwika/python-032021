a2b21a6aa9451252f1348471cbe1592bb295bf36
hedwika <pajerova.hedvika@gmail.com>
2021-10-08 17:52:19 +0200

    Úkol ze druhé lekce

--- 2/air_polution-domaci_ukol.py
+++ 2/air_polution-domaci_ukol.py
@@ -1,7 +1,8 @@
 import requests
 import pandas as pd
 import seaborn as sns
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy
 
 desired_width = 1000
 pd.set_option('display.width', desired_width)
@@ -24,13 +25,19 @@ air_polution["month"] = air_polution["date_converted"].dt.month
 
 # 3) Vytvoř pivot tabulku s průměrným počtem množství jemných částic (sloupec pm25) v jednotlivých měsících a jednotlivých letech.
 # Jako funkci pro agregaci můžeš použít numpy.mean.
-air_polution_aggregated_mean = air_polution.groupby(["year", "month"])["pm25"].mean()
-print(air_polution_aggregated_mean.head())
+air_polution_for_pivot = air_polution.drop(columns=['date', 'date_converted']).dropna()
+air_polution_aggregated_mean = air_polution_for_pivot.groupby(["year", "month"])["pm25"].mean()
+air_polution_aggregated_mean_df = pd.DataFrame(air_polution_aggregated_mean)
+air_polution_pivot = air_polution_aggregated_mean_df.pivot_table(index='month', columns="year", values="pm25",aggfunc=numpy.sum)
 
 # Dobrovolný doplněk
 # 1) Podívej se do první lekce na část o teplotních mapách a zobraz výsledek analýzy jako teplotní mapu.
-ax = sns.heatmap(air_polution_aggregated_mean, annot=True, fmt=".1f", linewidths=.5, cmap="YlGn")
+ax = sns.heatmap(air_polution_pivot, annot=True, fmt=".1f", linewidths=.5, cmap="YlGn")
 plt.show()
 
-# 2) Použij metodu dt.dayofweek a přidej si do sloupce den v týdnu. Číslování je od 0, tj. pondělí má číslo 0 a neděle 6.
-# Porovnej, jestli se průměrné množství jemných částic liší ve všední dny a o víkendu.
+# 2) Použij metodu dt.dayofweek a přidej si do sloupce den v týdnu. Číslování je od 0, tj. pondělí má číslo 0
+# a neděle 6. Porovnej, jestli se průměrné množství jemných částic liší ve všední dny a o víkendu.
+air_polution["day_of_the_week"] = air_polution["date_converted"].dt.dayofweek
+air_polution_for_pivot_days = air_polution.drop(columns=['date', 'date_converted', "year", "month"]).dropna()
+air_polution_aggregated_mean_days = air_polution_for_pivot_days.groupby(["day_of_the_week"])["pm25"].mean()
+air_polution_aggregated_mean_days_df = pd.DataFrame(air_polution_aggregated_mean_days)
