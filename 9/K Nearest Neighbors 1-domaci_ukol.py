9557033d3a80629076e574b378655034dd277df0
Hedvika Pajerová <pajerova.hedvika@gmail.com>
2021-11-18 23:11:05 +0100

    Lekce 9: Domácí úkol Neighbours, příklad 1

--- 9/K Nearest Neighbors 1-domaci_ukol.py
+++ 9/K Nearest Neighbors 1-domaci_ukol.py
@@ -5,11 +5,8 @@
 # V podstatě bude potřeba upravit krok 6. Upravení parametrů modelu. Na základě číselných hodnot nebo grafu vyber tu
 # hodnotu parametru, která dává nejlepší výsledek (nejvyšší hodnotu při volání precision()).
 
-import sklearn
 import pandas as pd
 import requests
-from nltk import recall
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
@@ -21,23 +18,15 @@ pd.set_option('display.width', desired_width)
 pd.set_option('display.max_columns',100)
 
 # 1) Definice problému: Je voda pitná, nebo ne? (dvě možnosti)
-
 r = requests.get("https://raw.githubusercontent.com/lutydlitatova/czechitas-datasets/main/datasets/water-potability.csv")
 open("water-potability.csv", 'wb').write(r.content)
 water_potability_df = pd.read_csv("water-potability.csv")
 
 # 2) Data a jejich příprava
 water_potability_df = water_potability_df.dropna()
-
-# zjištění disbalancí mezi hodnotami
-print(water_potability_df["Potability"].value_counts(normalize=True))
-
-# Rozdělení na features - sloupce, které určují, zda je pitná, nebo ne
 X = water_potability_df.drop(columns=["Potability"])
-# a target - cíl který chceme určit
 y = water_potability_df["Potability"]
 
-# rozdělení na testovací a trénovací data:
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 scaler = StandardScaler()
@@ -58,26 +47,6 @@ ConfusionMatrixDisplay.from_estimator(clf, X_test, y_test,
                                       cmap=plt.cm.Blues)
 
 plt.show()
-
-# True Positives (TP): To jsou data, která jsme označili jako třídu 1, v našem případně za pitnou vodu, a skutečně
-# se jednalo o pitnou vodu. Těch máme 97.
-# True Negatives (TN): To jsou data, která jsme označili jako třídu 0, v našem případě za nepitnou vodu, a skutečně
-# se jednalo o nepitnou vodu. Těch máme 283.
-# False Positives (FP): To jsou data, která jsme označili jako třídu 1, ale ve skutečnosti se jednalo o třídu 0.
-# O nepitné vodě jsme řekli, že je pitná. Takových případů máme 72.
-# False Negatives (FN): To jsou data, která jsme označili jako třídu 0, ale ve skutečnosti se jednalo o třídu 1.
-# O pitné vodě jsme řekli, že je nepitná. Takových případů máme 152.`
-
-# Jak teď spočítat úspěšnost modelu? Nabízí se několik metrik:
-# Accuracy: Poměr správně určených záznamů oproti celku.
-# $A = \frac{TP+TN}{TP+TN+FP+FN}$
-# Precision: Tato metrika penalizuje označení nepitné vody za pitnou.
-# $P = \frac{TP}{TP+FP}$
-# Recall: Tato metrika penalizuje označení pitné vody za nepitnou.
-# $R = \frac{TP}{TP+FN}$
-# F1 Score: Metrika která zohlední jak Precision, tak Recall.
-print(precision_score(y_test, y_pred))
-f1_score(y_test, y_pred)
 
 # 6. Upravení parametrů modelu, nastavení počtu sousedů:
 ks = list(range(1, 200))
