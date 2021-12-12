08a723d7e8e57ef76c8a56cc7f8173786d94e940
hedwika <pajerova.hedvika@gmail.com>
2021-12-11 15:02:29 -1000

    Lesson 11 - Silhouette homework .py

---
+++ 11-Clustering/silhouette_homework.py
@@ -0,0 +1,13 @@
+# V tomto úkolu nemusíš nic programovat.
+
+# Porovnej čtyři datasety vykreslené jako dvourozměrné bodové grafy (složka clusters) se čtyřmi grafy,
+# které zobrazují silhouette skóre těchto dat (složka silhouettes). Který dataset patří ke kterému grafu?
+#
+# Řešení je vždy dvojice číslo-písmeno, například 1-A, pokud bychom uvažovali, že dataset 1 patří k
+# silhouette grafu A.
+#
+# Řešení:
+# A-4
+# B-1
+# C-2
+# D-3
